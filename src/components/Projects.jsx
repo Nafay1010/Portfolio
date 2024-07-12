429630beed0fdb165d95f2ab453b46328e581cf4
Nafay1010 <abdulnafay53@gmail.com>
2024-07-12 16:05:50 +0500

    Projects Updated CV Updated

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -43,43 +43,6 @@ export const Projects = () => {
       <Container>
         <h1 className="mb-10">Live Projects</h1>
         <div className="grid grid-cols-1 gap-4 m-1 lg:grid-cols-2 place-items-center">
-          <a href="https://triform.ai/" target="_blank" rel="noreferrer">
-            <img
-              src={Triform_Img}
-              alt="Triform"
-              className="my-4 duration-200 ease-linear rounded-lg hover:opacity-70"
-            />
-          </a>
-          <div className="hidden space-y-2 lg:block">
-            <h6 className="flex items-center text-lg gap-x-1">
-              {" "}
-              <span>
-                <MdOutlineDateRange size={25} className="relative bottom-0.5" />
-              </span>
-              March 2024 - Present
-            </h6>
-            <p>
-              A DevOps platform for your Python based LLM-implementations. We
-              are committed to making scalable AI-implementation, integration
-              and orchestration available for everyone. Join us in our
-              alfa-testing and start exploring the simplicity of AI-integration!
-            </p>
-          </div>
-
-          <div className="space-y-2">
-            <h6 className="flex items-center text-lg gap-x-1">
-              {" "}
-              <span>
-                <MdOutlineDateRange size={25} className="relative bottom-0.5" />
-              </span>
-              June 2023 - Present
-            </h6>
-            <p>
-              A bearevment services platform which brings comfort and closure to
-              your loved ones after you have passed away by allowing them to
-              have interactive conversations with recordings of you.
-            </p>
-          </div>
           <a href="https://kinterak.com/" target="_blank" rel="noreferrer">
             <img
               src={Kinterak_Img}
@@ -87,20 +50,57 @@ export const Projects = () => {
               className="my-4 duration-200 ease-linear rounded-lg hover:opacity-70"
             />
           </a>
+          <div className="space-y-2">
+            {/* <h6 className="flex items-center text-lg gap-x-1">
+              {" "}
+              <span>
+                <MdOutlineDateRange size={25} className="relative bottom-0.5" />
+              </span>
+              June 2023 - Present
+            </h6> */}
+            <h6 className="text-lg">
+              A bearevment services platform which brings comfort and closure to
+              your loved ones after you have passed away by allowing them to
+              have interactive conversations with recordings of you.
+            </h6>
+          </div>
+          <div className="hidden space-y-2 lg:block">
+            {/* <h6 className="flex items-center text-lg gap-x-1">
+              {" "}
+              <span>
+                <MdOutlineDateRange size={25} className="relative bottom-0.5" />
+              </span>
+              March 2024 - Present
+            </h6> */}
+            <h6 className="text-lg">
+              A DevOps platform for your Python based LLM-implementations. We
+              are committed to making scalable AI-implementation, integration
+              and orchestration available for everyone. Join us in our
+              alfa-testing and start exploring the simplicity of AI-integration!
+            </h6>
+          </div>
+          <a href="https://triform.ai/" target="_blank" rel="noreferrer">
+            <img
+              src={Triform_Img}
+              alt="Triform"
+              className="my-4 duration-200 ease-linear rounded-lg hover:opacity-70"
+            />
+          </a>
+
           <div className="space-y-2 lg:hidden">
-            <h6 className="flex items-center text-lg gap-x-1">
+            {/* <h6 className="flex items-center text-lg gap-x-1">
               {" "}
               <span>
                 <MdOutlineDateRange size={25} className="relative bottom-0.5" />
               </span>
               March 2024 - Current
-            </h6>
-            <p>
+            </h6> */}
+            <h6 className="text-lg">
               A DevOps platform for your Python based LLM-implementations. We
               are committed to making scalable AI-implementation, integration
               and orchestration available for everyone. Join us in our
               alfa-testing and start exploring the simplicity of AI-integration!
-            </p>
+            </h6>
           </div>
           <a href="https://tobrecards.com/" target="_blank" rel="noreferrer">
             <img
@@ -110,32 +110,32 @@ export const Projects = () => {
             />
           </a>
           <div className="space-y-2">
-            <h6 className="flex items-center text-lg gap-x-1">
+            {/* <h6 className="flex items-center text-lg gap-x-1">
               {" "}
               <span>
                 <MdOutlineDateRange size={25} className="relative bottom-0.5" />
               </span>
               Nov 2023 - April 2024
-            </h6>
-            <p>
+            </h6> */}
+            <h6 className="text-lg">
               An AI based platform which generates personalized greeting cards
               for every occasion. It uses AI to generate the best possible cards
               for your recipients based on their preferences.
-            </p>
+            </h6>
           </div>
           <div className="hidden space-y-2 lg:block">
-            <h6 className="flex items-center text-lg gap-x-1">
+            {/* <h6 className="flex items-center text-lg gap-x-1">
               {" "}
               <span>
                 <MdOutlineDateRange size={25} className="relative bottom-0.5" />
               </span>
               August 2022 - Oct 2022
-            </h6>
-            <p>
+            </h6> */}
+            <h6 className="text-lg">
               A coffee shop website made for a client's startup. It is a UI
               design project that I made for a client from scratch using Figma
               and then implemented it in React and Tailwind CSS.
-            </p>
+            </h6>
           </div>
           <a
             href="https://coffee-website-delta.vercel.app/"
@@ -149,18 +149,18 @@ export const Projects = () => {
             />
           </a>
           <div className="space-y-2 lg:hidden">
-            <h6 className="flex items-center text-lg gap-x-1">
+            {/* <h6 className="flex items-center text-lg gap-x-1">
               {" "}
               <span>
                 <MdOutlineDateRange size={25} className="relative bottom-0.5" />
               </span>
               August 2022 - Oct 2022
-            </h6>
-            <p>
+            </h6> */}
+            <h6 className="text-lg">
               A coffee shop website made for a client's startup. It is a UI
               design project that I made for a client from scratch using Figma
               and then implemented it in React and Tailwind CSS.
-            </p>
+            </h6>
           </div>
           <a
             href="https://tenzies-project-olive.vercel.app/"
@@ -174,17 +174,17 @@ export const Projects = () => {
             />
           </a>
           <div className="space-y-2">
-            <h6 className="flex items-center text-lg gap-x-1">
+            {/* <h6 className="flex items-center text-lg gap-x-1">
               {" "}
               <span>
                 <MdOutlineDateRange size={25} className="relative bottom-0.5" />
               </span>
               Dec 2022 - Jan 2023
-            </h6>
-            <p>
+            </h6> */}
+            <h6 className="text-lg">
               A small game for a client. It is a simple game where you have to
               roll until all the numbers are same. This game is made with React.
-            </p>
+            </h6>
           </div>
         </div>
       </Container>
@@ -230,8 +230,8 @@ export const Projects = () => {
                     <div className="card-info-title">
                       <h3>User-Trained Chatbot</h3>
                       <h4>
-                        Developed using NEXTJS 13 powered by Llama Index &
-                        GPT-4 Model.
+                        Developed using NEXTJS 13 powered by Llama Index & GPT-4
+                        Model.
                       </h4>
                     </div>
                   </div>
