4f0e4562a54bf8617db55afd9abddf1c8d36e7d5
Nafay1010 <abdulnafay53@gmail.com>
2024-11-23 20:20:27 +0500

    Google Specialization Added

--- src/components/MetaCertification.jsx
+++ src/components/MetaCertification.jsx
@@ -1,5 +1,6 @@
 import colorSharp from "../assets/img/color-sharp.png";
 import Certificate from "../assets/img/Certificate.png";
+import GoogleCertificate from "../assets/img/GoogleCertificate.png";
 
 export const MetaCertification = () => {
   return (
@@ -18,6 +19,17 @@ export const MetaCertification = () => {
           src={Certificate}
           alt="Meta Certificate"
           className="w-[80%] rounded-lg hover:opacity-80 duration-150 ease-linear"
+        />
+      </a>
+      <a
+        href="https://www.coursera.org/account/accomplishments/specialization/certificate/2OF8LBAHFQDD"
+        className="meta-container"
+        target="_blank"
+      >
+        <img
+          src={GoogleCertificate}
+          alt="Google Certificate"
+          className="w-[80%] mt-10 rounded-lg hover:opacity-80 duration-150 ease-linear"
         />
       </a>
     </section>
